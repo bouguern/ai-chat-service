f8ac938801b385da57e073e359a85c23691a7757
Bouguern Mohamed <bouguern.mohamed.10@gmail.com>
2026-04-15 11:26:33 +0100

    Initial commit

---
+++ src/main/java/bouguern/langchain4j/demo/Java21Springboot3Langchain4jOllamaApplication.java
@@ -0,0 +1,13 @@
+package bouguern.langchain4j.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21Springboot3Langchain4jOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21Springboot3Langchain4jOllamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bouguern/langchain4j/demo/Java21Springboot3Langchain4jOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package bouguern.langchain4j.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21Springboot3Langchain4jOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
